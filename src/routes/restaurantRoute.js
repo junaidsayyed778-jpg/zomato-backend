890b1658432585b00ab0bd1e4444acf61a6f4a0d
Junaid Sayyed <junaidsayyed771@gmail.com>
2026-01-29 15:47:52 +0530

    add restaurant schema

--- src/routes/restaurantRoute.js
+++ src/routes/restaurantRoute.js
@@ -1,6 +1,16 @@
 import express from "express";
+import { getAllRestaurant } from "../controllers/restaurantController";
+import { protect } from "../middlewares/authMiddleware";
+import { restrictTo } from "../middlewares/roleMiddleware";
+import { zodValidate } from "../middlewares/zodValidate";
 
 const router = express.Router();
 
 //PUBLIC - list restaurants
-router.get("/", get)
+router.get("/", getAllRestaurant);
+
+//PROTECTED - create restaurant
+router.post("/", protect,
+    restrictTo("ADMIN", "RESTURANT_OWNER"),
+    zodValidate(crete)
+)
---
+++ src/validators/restaurantSchema.js
@@ -0,0 +1,12 @@
+import {z} from "zod";
+
+export const createRestaurantSchema = z.object({
+    name: z.string().min(2),
+    location: z.string().min(2)
+});
+
+export const updateRestaurantSchema = z.object({
+    name: z.string().min(2).optional(),
+    location: z.string().min(2).optional(),
+    isOpen: z.boolean().optional(),
+});
