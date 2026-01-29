fb4aa5a71ac80ded44c553f8b353741767656a5c
Junaid Sayyed <junaidsayyed771@gmail.com>
2026-01-29 14:33:40 +0530

    add order route

---
+++ src/routes/orderRoutes.js
@@ -0,0 +1,10 @@
+import express from "express";
+import { zodValidate } from "../middlewares/zodValidate.js";
+import { createOrderSchema } from "../validators/orderSchema.js";
+
+const router = express.Router();
+
+router.post("/", zodValidate(createOrderSchema),
+createOrderSchema);
+
+export default router;
