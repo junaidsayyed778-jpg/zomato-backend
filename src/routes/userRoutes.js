7f9a9892013eae542ec00bb317026e107ee3db0d
Junaid Sayyed <junaidsayyed771@gmail.com>
2026-01-29 15:13:09 +0530

    add user route

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,5 +1,12 @@
 import express from "express";
+import { getProfile, updateProfile } from "../controllers/userController";
+import { protect } from "../middlewares/authMiddleware";
 
 const router = express.Router();
 
-router.get("/me", protect)
+router.get("/me", protect, getProfile);
+
+//PUT /api/users/me
+router.put("/me", protect, updateProfile);
+
+export default router;
