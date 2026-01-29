34e62bd73b2cb29883aa6e8dea55f89e7a9814bd
Junaid Sayyed <junaidsayyed771@gmail.com>
2026-01-29 15:51:55 +0530

    add restaurant routes

--- src/routes/restaurantRoute.js
+++ src/routes/restaurantRoute.js
@@ -1,8 +1,10 @@
 import express from "express";
-import { getAllRestaurant } from "../controllers/restaurantController";
+import { createRestaurant, getAllRestaurant, updateRestaurant } from "../controllers/restaurantController";
 import { protect } from "../middlewares/authMiddleware";
 import { restrictTo } from "../middlewares/roleMiddleware";
 import { zodValidate } from "../middlewares/zodValidate";
+import { createOrderSchema } from "../validators/orderSchema";
+
 
 const router = express.Router();
 
@@ -12,5 +14,17 @@ router.get("/", getAllRestaurant);
 //PROTECTED - create restaurant
 router.post("/", protect,
     restrictTo("ADMIN", "RESTURANT_OWNER"),
-    zodValidate(crete)
-)
+    zodValidate(createOrderSchema),
+    createRestaurant
+);
+
+//PROTECTED - update restaurant
+router.put(
+    "/:id",
+    protect,
+    restrictTo("ADMIN", "RESTURANT_OWNER"),
+    zodValidate(createRestaurantSchema),
+    updateRestaurant
+);
+
+export default router;
