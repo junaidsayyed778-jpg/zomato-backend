bdf8f45965d9f1abf62e707b13211d0a09d50adf
Junaid Sayyed <junaidsayyed771@gmail.com>
2026-01-31 16:15:56 +0530

    update index routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,11 @@
 import authRoutes from "./authRoutes.js";
 import userRoutes from "./userRoutes.js";
 import restaurantRoutes from "./restaurantRoute.js";
+import cartRoutes from "./cartRoutes.js"
 
 export const loadRoutes = (app) => {
     app.use("/api/auth", authRoutes);
     app.use("/api/users", userRoutes);
-    app.use("/api/restaurants", restaurantRoutes)
+    app.use("/api/cart", cartRoutes)
+    app.use("/api/restaurants", restaurantRoutes);
 };
